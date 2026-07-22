bd76c1989503ec5b5310c889f405e7b37cd39a64
maledziekco <btomasz166@gmail.com>
2026-07-22 15:16:52 +0200

    naprawa bledu

--- src/main/java/pl/betterraid/ConfigManager.java
+++ src/main/java/pl/betterraid/ConfigManager.java
@@ -1,96 +1,40 @@
 package pl.betterraid;
 
-import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
 
 public class ConfigManager {
 
     private final BetterRaid plugin;
 
-    private double healthMultiplier;
-    private double damageMultiplier;
-    private int extraMobsMultiplier;
-    private double eliteSpawnChance;
-    private String eliteHelmet;
-    private String eliteChestplate;
-    private String eliteWeapon;
-    private String elitePrefix;
-
-    private boolean bossEnabled;
-    private String bossType;
-    private String bossName;
-    private double bossMaxHealth;
-    private String bossBarColor;
-    private String bossBarStyle;
-
-    private String prefix;
-    private String noPermissionMsg;
-    private String configReloadedMsg;
-    private String raidStartedMsg;
-    private String bossSpawnedMsg;
-    private String bossDefeatedMsg;
-
     public ConfigManager(BetterRaid plugin) {
         this.plugin = plugin;
-        reloadConfig();
+        plugin.saveDefaultConfig();
     }
 
     public void reloadConfig() {
-        plugin.saveDefaultConfig();
         plugin.reloadConfig();
+    }
+
+    public int getExtraMobsMultiplier() {
+        return plugin.getConfig().getInt("raid.extra-mobs-multiplier", 2);
+    }
+
+    public double getDamageMultiplier() {
+        return plugin.getConfig().getDouble("raid.damage-multiplier", 2.5);
+    }
+
+    public double getHealthMultiplier() {
+        return plugin.getConfig().getDouble("raid.health-multiplier", 1.0);
+    }
+
+    public double getMobBaseHealth(EntityType type) {
         FileConfiguration config = plugin.getConfig();
-
-        this.healthMultiplier = config.getDouble("raid.health-multiplier", 1.50);
-        this.damageMultiplier = config.getDouble("raid.damage-multiplier", 2.50);
-        this.extraMobsMultiplier = config.getInt("raid.extra-mobs-multiplier", 2);
-
-        this.eliteSpawnChance = config.getDouble("elites.spawn-chance", 0.30);
-        this.eliteHelmet = config.getString("elites.helmet", "DIAMOND_HELMET");
-        this.eliteChestplate = config.getString("elites.chestplate", "DIAMOND_CHESTPLATE");
-        this.eliteWeapon = config.getString("elites.weapon", "NETHERITE_SWORD");
-        this.elitePrefix = config.getString("elites.display-prefix", "&e[ELITA] ");
-
-        this.bossEnabled = config.getBoolean("boss.enabled", true);
-        this.bossType = config.getString("boss.type", "RAVAGER");
-        this.bossName = config.getString("boss.name", "&c&lBOSS");
-        this.bossMaxHealth = config.getDouble("boss.max-health", 500.0);
-        this.bossBarColor = config.getString("boss.bossbar-color", "RED");
-        this.bossBarStyle = config.getString("boss.bossbar-style", "SOLID");
-
-        this.prefix = colorize(config.getString("messages.prefix", "&8[&cBetterRaid&8] "));
-        this.noPermissionMsg = colorize(prefix + config.getString("messages.no-permission", "&cBrak uprawnień!"));
-        this.configReloadedMsg = colorize(prefix + config.getString("messages.config-reloaded", "&aPrzeładowano config!"));
-        this.raidStartedMsg = colorize(prefix + config.getString("messages.raid-started", "&aRozpoczęto rajd!"));
-        this.bossSpawnedMsg = colorize(prefix + config.getString("messages.boss-spawned", "&cZespawnowano Bossa!"));
-        this.bossDefeatedMsg = colorize(prefix + config.getString("messages.boss-defeated", "&aBoss został pokonany!"));
+        String path = "mobs-base-hp." + type.name();
+        
+        if (config.contains(path)) {
+            return config.getDouble(path);
+        }
+        return 24.0; // Domyślna wartość, jeśli brak wpisu w configu
     }
-
-    public String colorize(String message) {
-        if (message == null) return "";
-        return ChatColor.translateAlternateColorCodes('&', message);
-    }
-
-    // Getters
-    public double getHealthMultiplier() { return healthMultiplier; }
-    public double getDamageMultiplier() { return damageMultiplier; }
-    public int getExtraMobsMultiplier() { return extraMobsMultiplier; }
-    public double getEliteSpawnChance() { return eliteSpawnChance; }
-    public String getEliteHelmet() { return eliteHelmet; }
-    public String getEliteChestplate() { return eliteChestplate; }
-    public String getEliteWeapon() { return eliteWeapon; }
-    public String getElitePrefix() { return elitePrefix; }
-
-    public boolean isBossEnabled() { return bossEnabled; }
-    public String getBossType() { return bossType; }
-    public String getBossName() { return bossName; }
-    public double getBossMaxHealth() { return bossMaxHealth; }
-    public String getBossBarColor() { return bossBarColor; }
-    public String getBossBarStyle() { return bossBarStyle; }
-
-    public String getPrefix() { return prefix; }
-    public String getNoPermissionMsg() { return noPermissionMsg; }
-    public String getConfigReloadedMsg() { return configReloadedMsg; }
-    public String getRaidStartedMsg() { return raidStartedMsg; }
-    public String getBossSpawnedMsg() { return bossSpawnedMsg; }
-    public String getBossDefeatedMsg() { return bossDefeatedMsg; }
 }
--- src/main/java/pl/betterraid/RaidManager.java
+++ src/main/java/pl/betterraid/RaidManager.java
@@ -1,40 +1,12 @@
 package pl.betterraid;
 
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.EntityType;
-
-public class ConfigManager {
+public class RaidManager {
 
     private final BetterRaid plugin;
 
-    public ConfigManager(BetterRaid plugin) {
+    public RaidManager(BetterRaid plugin) {
         this.plugin = plugin;
-        plugin.saveDefaultConfig();
     }
 
-    public void reloadConfig() {
-        plugin.reloadConfig();
-    }
-
-    public int getExtraMobsMultiplier() {
-        return plugin.getConfig().getInt("raid.extra-mobs-multiplier", 2);
-    }
-
-    public double getDamageMultiplier() {
-        return plugin.getConfig().getDouble("raid.damage-multiplier", 2.5);
-    }
-
-    public double getHealthMultiplier() {
-        return plugin.getConfig().getDouble("raid.health-multiplier", 1.0);
-    }
-
-    public double getMobBaseHealth(EntityType type) {
-        FileConfiguration config = plugin.getConfig();
-        String path = "mobs-base-hp." + type.name();
-        
-        if (config.contains(path)) {
-            return config.getDouble(path);
-        }
-        return 24.0; // Domyślna wartość, jeśli brak wpisu w configu
-    }
+    // Twoja logika zarządzania rajdami i bossem
 }
