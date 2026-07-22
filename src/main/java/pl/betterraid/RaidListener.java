4672bc93bbe99c6bbeb143aeaeebd8087e48a677
maledziekco <btomasz166@gmail.com>
2026-07-22 14:11:17 +0200

    poprawki

--- src/main/java/pl/betterraid/RaidListener.java
+++ src/main/java/pl/betterraid/RaidListener.java
@@ -1,8 +1,12 @@
 package pl.betterraid;
 
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Raider;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
 
 public class RaidListener implements Listener {
 
@@ -13,7 +17,29 @@ public class RaidListener implements Listener {
     }
 
     @EventHandler
+    public void onRaidSpawn(EntitySpawnEvent event) {
+        // Sprawdzamy, czy zespawnowany mob to uczestnik rajdu (Raider)
+        if (event.getEntity() instanceof Raider raider) {
+            
+            // Ignorujemy moby poza aktywnym rajdem
+            if (raider.getRaid() == null) return;
+
+            // Pobieramy szansę z configu
+            double chance = plugin.getConfigManager().getExtraMobsChance();
+
+            // Jeśli losowa wartość jest mniejsza niż szansa - klonujemy moba!
+            if (Math.random() < chance) {
+                World world = raider.getWorld();
+                Location loc = raider.getLocation();
+                
+                // Spawnuje dodatkowego moba tego samego typu w tym samym miejscu
+                world.spawn(loc, raider.getClass());
+            }
+        }
+    }
+
+    @EventHandler
     public void onEntityDeath(EntityDeathEvent event) {
-        // Logika zdarzen
+        // Logika zdarzeń po śmierci moba
     }
 }
