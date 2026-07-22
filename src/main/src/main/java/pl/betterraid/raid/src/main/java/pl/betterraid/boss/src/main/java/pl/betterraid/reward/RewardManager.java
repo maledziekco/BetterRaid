8b7e30e35ff487441ffb65e7d1ad4fb7a3248777
maledziekco <btomasz166@gmail.com>
2026-07-22 13:09:04 +0200

    Create RewardManager.java

---
+++ src/main/src/main/java/pl/betterraid/raid/src/main/java/pl/betterraid/boss/src/main/java/pl/betterraid/reward/RewardManager.java
@@ -0,0 +1,11 @@
+package pl.betterraid.reward;
+
+import pl.betterraid.BetterRaid;
+
+public class RewardManager {
+    private final BetterRaid plugin;
+
+    public RewardManager(BetterRaid plugin) {
+        this.plugin = plugin;
+    }
+}
