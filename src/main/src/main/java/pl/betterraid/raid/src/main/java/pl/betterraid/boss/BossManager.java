87b6252eb7ef92cf382096e527d191de63666df4
maledziekco <btomasz166@gmail.com>
2026-07-22 13:08:51 +0200

    Create BossManager.java

---
+++ src/main/src/main/java/pl/betterraid/raid/src/main/java/pl/betterraid/boss/BossManager.java
@@ -0,0 +1,11 @@
+package pl.betterraid.boss;
+
+import pl.betterraid.BetterRaid;
+
+public class BossManager {
+    private final BetterRaid plugin;
+
+    public BossManager(BetterRaid plugin) {
+        this.plugin = plugin;
+    }
+}
