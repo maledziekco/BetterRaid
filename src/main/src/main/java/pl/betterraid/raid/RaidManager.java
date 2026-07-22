eb375c367badfaef0cbe5bfb7e30408114176d43
maledziekco <btomasz166@gmail.com>
2026-07-22 13:08:32 +0200

    Create RaidManager.java

---
+++ src/main/src/main/java/pl/betterraid/raid/RaidManager.java
@@ -0,0 +1,11 @@
+package pl.betterraid.raid;
+
+import pl.betterraid.BetterRaid;
+
+public class RaidManager {
+    private final BetterRaid plugin;
+
+    public RaidManager(BetterRaid plugin) {
+        this.plugin = plugin;
+    }
+}
