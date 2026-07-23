45fbd2745c7a04dff027bab6b747ee35c301d4c2
maledziekco <btomasz166@gmail.com>
2026-07-23 22:22:36 +0200

    ss

--- src/main/java/pl/betterraid/BetterRaid.java
+++ src/main/java/pl/betterraid/BetterRaid.java
@@ -1,22 +1,42 @@
 package pl.betterraid;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import pl.betterraid.commands.BetterRaidCommand;
 import pl.betterraid.commands.BroadcastCommand;
 
 public final class BetterRaid extends JavaPlugin {
 
+    private ConfigManager configManager;
+    private BossManager bossManager;
+
     @Override
     public void onEnable() {
-        // Rejestracja komendy /ogloszenie
+        // Inicjalizacja menedżerów
+        this.configManager = new ConfigManager(this);
+        this.bossManager = new BossManager(this);
+
+        // Rejestracja komend
         if (getCommand("ogloszenie") != null) {
             getCommand("ogloszenie").setExecutor(new BroadcastCommand());
         }
-        
+        if (getCommand("betterraid") != null) {
+            getCommand("betterraid").setExecutor(new BetterRaidCommand(this));
+        }
+
         getLogger().info("Plugin BetterRaid został pomyślnie uruchomiony!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Plugin BetterRaid został wyłączony.");
+    }
+
+    // Metody getters wymagane przez RaidListener oraz BetterRaidCommand
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public BossManager getBossManager() {
+        return bossManager;
     }
 }
