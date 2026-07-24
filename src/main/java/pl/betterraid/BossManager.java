f671dda3b51639817ea6a16b9847604a0636111c
maledziekco <btomasz166@gmail.com>
2026-07-24 20:10:47 +0200

    hp

--- src/main/java/pl/betterraid/BossManager.java
+++ src/main/java/pl/betterraid/BossManager.java
@@ -1,16 +1,17 @@
-package pl.betterraid;
+packaimport org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.LivingEntity;
 
-import org.bukkit.Location;
-
-public class BossManager {
-
-    private final BetterRaid plugin;
-
-    public BossManager(BetterRaid plugin) {
-        this.plugin = plugin;
-    }
-
-    public void spawnBoss(Location location) {
-        // Logika spawnowania bossa
+// Przykład w metodzie spawnowania moba/bossa:
+public void setupCustomHealth(LivingEntity entity, double maxHealth) {
+    // 1. Pobieramy atrybut maksymalnego zdrowia entity
+    AttributeInstance healthAttribute = entity.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+    
+    if (healthAttribute != null) {
+        // 2. Ustawiamy maksymalne zdrowie (np. z configu)
+        healthAttribute.setBaseValue(maxHealth);
+        
+        // 3. Ustawiamy aktualne zdrowie na maksimum, żeby mob nie spawned się "ranny"
+        entity.setHealth(maxHealth);
     }
 }
