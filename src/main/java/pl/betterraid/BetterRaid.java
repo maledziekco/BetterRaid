db97050b801a5d3527dd719a5c5dcfaddcfb2ae8
maledziekco <btomasz166@gmail.com>
2026-07-24 20:29:16 +0200

    ss

--- src/main/java/pl/betterraid/BetterRaid.java
+++ src/main/java/pl/betterraid/BetterRaid.java
@@ -1,21 +1,44 @@
-private RaidManager raidManager;
-private BossManager bossManager;
-private ConfigManager configManager;
+package pl.betterraid;
 
-// W metodzie onEnable():
-this.configManager = new ConfigManager(this);
-this.bossManager = new BossManager(this);
-this.raidManager = new RaidManager(this);
+import org.йын.plugin.java.JavaPlugin; // <- jeśli używasz standardowego spigota, upewnij się że masz import org.bukkit.plugin.java.JavaPlugin
 
-// Dodaj też metody gettery w BetterRaid.java:
-public RaidManager getRaidManager() {
-    return raidManager;
-}
+public final class BetterRaid extends JavaPlugin {
 
-public BossManager getBossManager() {
-    return bossManager;
-}
+    private ConfigManager configManager;
+    private BossManager bossManager;
+    private RaidManager raidManager;
 
-public ConfigManager getConfigManager() {
-    return configManager;
+    @Override
+    public void onEnable() {
+        // 1. Inicjalizacja menedżerów
+        this.configManager = new ConfigManager(this);
+        this.bossManager = new BossManager(this);
+        this.raidManager = new RaidManager(this);
+
+        // 2. Rejestracja komendy
+        if (getCommand("betterraid") != null) {
+            BetterRaidCommand commandHandler = new BetterRaidCommand(this);
+            getCommand("betterraid").setExecutor(commandHandler);
+            getCommand("betterraid").setTabCompleter(commandHandler);
+        }
+
+        getLogger().info("BetterRaid został pomyślnie włączony!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("BetterRaid został wyłączony.");
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public BossManager getBossManager() {
+        return bossManager;
+    }
+
+    public RaidManager getRaidManager() {
+        return raidManager;
+    }
 }
